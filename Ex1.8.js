74892e091c2225007c0941256ef9ba46ce163f46
Source Academy User <No public email provided>
2024-09-28 02:21:47 +0800

    its 2 am and im tired

--- Ex1.8.js
+++ Ex1.8.js
@@ -1,1 +1,30 @@
 
+function abs(x) {
+    return x >= 0 ? x : - x;
+}
+
+function square(x) {
+    return x * x;
+}
+
+function is_good_enough(guess, newGuess) {
+    return abs(-guess + newGuess) / guess < 0.0001;
+}
+
+function improve(guess, x) {
+    return ((x / (guess * guess) + (guess * 2))) / 3;
+}
+
+function sqrt_iter(guess, x) {
+    const newGuess = improve(guess,x);
+    return is_good_enough(guess, newGuess)
+          ? guess
+          : sqrt_iter(newGuess, x);
+}
+
+
+display(sqrt_iter(1, 8)); // 2
+display(sqrt_iter(1, 27)); // 3
+display(sqrt_iter(1, 64)); // 4
+display(sqrt_iter(1, 125)); // 5
+display(sqrt_iter(4, 4096)); // 5
