0ee0d4e05f9649fca3baf892b4374911b2edcd9c
Source Academy User <No public email provided>
2024-09-28 09:32:58 +0800

    Spring cleaning

--- Ex1.8.js
+++ Ex1.8.js
@@ -3,28 +3,24 @@ function abs(x) {
     return x >= 0 ? x : - x;
 }
 
-function square(x) {
-    return x * x;
-}
-
 function is_good_enough(guess, newGuess) {
-    return abs(-guess + newGuess) / guess < 0.0001;
+    return abs(-guess + newGuess) / guess < 0.00000001;
 }
 
-function improve(guess, x) {
+function improvedCube(guess, x) {
     return ((x / (guess * guess) + (guess * 2))) / 3;
 }
 
-function sqrt_iter(guess, x) {
-    const newGuess = improve(guess,x);
+function cube_iter(guess, x) {
+    const newGuess = improvedCube(guess,x);
     return is_good_enough(guess, newGuess)
           ? guess
-          : sqrt_iter(newGuess, x);
+          : cube_iter(newGuess, x);
 }
 
 
-display(sqrt_iter(1, 8)); // 2
-display(sqrt_iter(1, 27)); // 3
-display(sqrt_iter(1, 64)); // 4
-display(sqrt_iter(1, 125)); // 5
-display(sqrt_iter(4, 4096)); // 5
+display(cube_iter(1, 8)); // 2
+display(cube_iter(1, 27)); // 3
+display(cube_iter(1, 64)); // 4
+display(cube_iter(1, 125)); // 5
+display(cube_iter(4, 4096)); // 16
