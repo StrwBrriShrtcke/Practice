058fac06bcf0d9c3864c406f55dfa496f5cffa9d
Source Academy User <No public email provided>
2024-09-28 22:01:58 +0800

    triangle

--- factorial.js
+++ factorial.js
@@ -1,17 +1,12 @@
 // n * (n - 1) * (n - 2) * ... * 1
-function factorial(n) {
-    return n === 0
-    ? 1
-    : n * factorial(n -1);
-}
-
-// function nZero(n) {
+// function factorial(n) {
 //     return n === 0
 //     ? 1
-//     : factorial(n);
+//     : n * factorial(n -1);
 // }
 
-display(factorial(5));
+
+// display(factorial(5));
 
 // n + (n-1) + (n-2) + ... + 1
 // function triangle(n) {
@@ -23,6 +18,17 @@ display(factorial(5));
 // display(triangle(3));
 
 
+
 function triangle(n) {
-    return 
+    function  triangle_iter(product, counter, max_count) {
+        return counter > max_count
+             ? product
+             : triangle_iter(product + counter,
+                          counter + 1,
+                          max_count);
+    }
+     return triangle_iter(0, 1, n);
 }
+triangle(9);
+                       
+                     
