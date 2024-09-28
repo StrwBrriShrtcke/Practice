372ed6ada442b49819364e76725a57a08b227a4b
Source Academy User <No public email provided>
2024-09-28 18:24:39 +0800

    Changes made from Source Academy

--- factorial.js
+++ factorial.js
@@ -1,1 +1,23 @@
+// n * (n - 1) * (n - 2) * ... * 1
+function factorial(n) {
+    return n === 0
+    ? 1
+    : n * factorial(n -1);
+}
 
+// function nZero(n) {
+//     return n === 0
+//     ? 1
+//     : factorial(n);
+// }
+
+display(factorial(5));
+
+// n + (n-1) + (n-2) + ... + 1
+// function triangle(n) {
+//     return n === 1
+//     ? 1
+//     : n + triangle(n - 1);
+// }
+
+// display(triangle(3));
