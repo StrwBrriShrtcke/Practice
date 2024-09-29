26879d4734fa176409e6014b9281a394b8c37dfb
Source Academy User <No public email provided>
2024-09-29 08:47:56 +0800

    Changes made from Source Academy

--- pyramid_factorial.js
+++ pyramid_factorial.js
@@ -1,1 +1,10 @@
-//hi
+function pyramid(n) {
+    function pyramid_inter(product, counter) {
+        return counter > n
+        ? product
+        :pyramid_inter((product * product - counter), counter + 1);
+    }
+    return pyramid_inter(1, 0);
+}
+
+pyramid(3);
