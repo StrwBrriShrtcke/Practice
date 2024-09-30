6591bcb96054b35c7a946df9d8c3ec27342306c4
Source Academy User <No public email provided>
2024-09-30 21:46:41 +0800

    Changes made from Source Academy

--- string.js
+++ string.js
@@ -1,1 +1,59 @@
 
+
+
+
+function length1(s) {
+    function iter(counter) {
+        return is_undefined(char_at(s, counter))
+        ? counter 
+        : iter(counter + 1);
+    }
+    return iter(0);
+}
+
+
+
+function length2(s) {
+    function iter(counter) {
+        return is_undefined(char_at(s, counter))
+        ? counter 
+        : iter(counter + 10);
+    }
+    function iter2(s)
+    const upperbound = iter(0);
+}
+
+// display(length("hello"));
+// display(length("cat"));
+// display(length("unicorn"));
+// display(length(""));
+// display(length(""));
+display(length("Lorem ipsum odor amet, consectetuer adipiscing elit. Commodo habitant vitae condimentum nisl nisi diam ornare. Litora in ex sodales hendrerit quis finibus maecenas dignissim. Gravida ad vel tempus vestibulum curabitur sollicitudin. Iaculis aliquam congue interdum ullamcorper mi massa inceptos. Amet egestas a, risus ultrices torquent himenaeos.Ad habitant porta finibus a praesent libero ullamcorper senectus. Litora nibh suspendisse aliquet hac parturient nec. Egestas nisi sollicitudin semper ipsum eget magna facilisis dignissim justo. Natoque velit sodales dolor donec a porta aliquet lacus. Venenatis nibh maecenas posuere pulvinar vestibulum dignissim purus. Pretium ullamcorper lectus fermentum nam justo, quam etiam nibh. Scelerisque curabitur ultricies ipsum sagittis viverra."));
+
+
+// function factorial(n) {
+//     function iter(product, counter) {
+//         return counter > n 
+//               ? product
+//               : iter(counter * product,
+//                       counter + 1);
+//     }
+//     return iter(1, 1);
+// }
+
+// factorial(5);
+
+
+
+
+
+// function length(s) {
+//     function iter(counter) {
+//         return is_undefined(char_at(s, iter))
+//         ? counter - 1
+//         : iter(counter + 1);
+//     }
+//     return iter(0);
+// }
+
+// length("hello");
