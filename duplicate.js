d97b3b799bd4c4dd946e71eeb42bdad10797e7d1
Source Academy User <No public email provided>
2024-10-04 18:55:25 +0800

    Changes made from Source Academy

--- duplicate.js
+++ duplicate.js
@@ -1,8 +1,8 @@
 function duplicate_string(s, n) {
     function iter(r, counter) {
         return counter >= n 
-        ? s
-        :s + iter(r, counter + 1);
+        ? r
+        :iter(r + s, counter + 1);
     }
     return n === 0 ? "" : iter(s, 1);
 }
