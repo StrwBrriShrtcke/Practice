9f94ed46584c92e694c50ca2fe24b97989c3c4ee
Source Academy User <No public email provided>
2024-10-04 20:21:09 +0800

    Changes made from Source Academy

--- fib.js
+++ fib.js
@@ -1,1 +1,13 @@
+function num_of_ways_to_step(step) {
+    function iter(a, b, count) {
+        return count === 0
+           ? b
+           : iter(a + b, a, count - 1);
+    }
+    return iter(1, 1, step);
+}
 
+
+display(num_of_ways_to_step(1)); //1
+display(num_of_ways_to_step(2)); //2
+display(num_of_ways_to_step(3)); //3
