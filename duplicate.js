cfa804c9113d046fb5573fc1d75e69021826d8e7
Source Academy User <No public email provided>
2024-10-04 19:31:40 +0800

    Changes made from Source Academy

--- duplicate.js
+++ duplicate.js
@@ -1,13 +1,47 @@
-function duplicate_string(s, n) {
-    function iter(r, counter) {
-        return counter >= n 
-        ? r
-        :iter(r + s, counter + 1);
+// function duplicate_string(s, n) {
+//     function iter(r, counter) {
+//         return counter >= n 
+//         ? r
+//         :iter(r + s, counter + 1);
+//     }
+//     return iter("", 0);
+// }
+
+// display(duplicate_string("a", 5));
+// display(duplicate_string("hah", 2));
+// display(duplicate_string("abcd", 0));
+// display(duplicate_string("", 10));
+
+
+function duplicate_string_space(s, n) {
+    // function iter(result, counter) {
+    //     return counter >= n
+    //     ? result
+    //     : counter < 1 
+    //     ? iter(result + s, counter + 1)
+    //     : iter(result + " " + s, counter + 1);
+    // }
+    function iter(result, counter) {
+        return counter >= n
+        ? result
+        : iter(counter < 1 ? result + s : result + " " + s, counter + 1);
     }
     return iter("", 0);
 }
 
-display(duplicate_string("a", 5));
-display(duplicate_string("hah", 2));
-display(duplicate_string("abcd", 0));
-display(duplicate_string("", 10));
+display(duplicate_string_space("a", 5));
+display(duplicate_string_space("hah", 2));
+display(duplicate_string_space("abcd", 0));
+display(duplicate_string_space("", 10));
+
+
+function duplicate_string_with_separator(s, n, sep) {
+    function iter(result, counter) {
+        return counter >=n
+    ? result 
+    : iter(counter < 1 ? result + s : result + sep + s, counter + 1);
+    
+    }
+    return iter("", 0);
+}
+display(duplicate_string_with_separator("a", -1, "/"));
