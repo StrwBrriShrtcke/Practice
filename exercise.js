5d0d61a71d473d7839d1583fe1b3028268a836f3
Source Academy User <No public email provided>
2024-10-09 13:26:25 +0800

    Changes made from Source Academy

--- exercise.js
+++ exercise.js
@@ -1,19 +1,33 @@
-function recursive_function(n) { 
-    return n < 3
-    ? n
-    : recursive_function(n - 1) + 2 * recursive_function(n - 2) 
-    + 3 * recursive_function(n - 3);
+// function recursive_function(n) { 
+//     return n < 3
+//     ? n
+//     : recursive_function(n - 1) + 2 * recursive_function(n - 2) 
+//     + 3 * recursive_function(n - 3);
+// }
+
+
+// display(recursive_function(0)); //0
+// display(recursive_function(1)); //1
+// display(recursive_function(2)); //2
+// display(recursive_function(3)); //4
+// display(recursive_function(4)); //11
+// display(recursive_function(5)); //25
+
+
+function iterative_function(n) {
+    function iter(n) {
+        return iter(n - 1) + 2 * iter(n - 2) 
+    + 3 * iter(n - 3);
+    }
+    return n < 3 ? n : iter(n);
 }
 
-
-display(recursive_function(0)); //0
-display(recursive_function(1)); //1
-display(recursive_function(2)); //2
-display(recursive_function(3)); //4
-display(recursive_function(4)); //11
-display(recursive_function(5)); //25
-
-
+display(iterative_function(0)); //0
+display(iterative_function(1)); //1
+display(iterative_function(2)); //2
+display(iterative_function(3)); //4
+display(iterative_function(4)); //11
+display(iterative_function(5)); //25
 
 // f(n−1)+2f(n−2)+3f(n−3)
 
