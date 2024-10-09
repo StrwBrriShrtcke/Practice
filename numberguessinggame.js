2111c471e8a4acc738da346f7dedf8d4525cb609
Source Academy User <No public email provided>
2024-10-09 18:30:26 +0800

    Changes made from Source Academy

--- numberguessinggame.js
+++ numberguessinggame.js
@@ -1,21 +1,34 @@
-// function throwdie() {
-//     return math_floor(math_random() * 6) + 1;
-// }
-
-function two_digit() {
-    return math_floor(math_random() * 89) + 10;
+function throwdie() {
+    return math_floor(math_random() * 6) + 1;
 }
-
-function test_two_digit(counter) {
+function test_throw_die(counter) {
     function test(counter) {
-        const n = two_digit();
+        const n = throwdie();
         display(stringify(n) + " | " + stringify(counter));
         return counter === 1
         ? true
-        : n < 10  || n >= 100 
+        : n < 1  || n >= 7
         ? false
         : test(counter - 1);
     }
     return test(counter);
 }
-test_two_digit(2000);
+test_throw_die(1000);
+
+// function two_digit() {
+//     return math_floor(math_random() * 89) + 10;
+// }
+
+// function test_two_digit(counter) {
+//     function test(counter) {
+//         const n = two_digit();
+//         display(stringify(n) + " | " + stringify(counter));
+//         return counter === 1
+//         ? true
+//         : n < 10  || n >= 100 
+//         ? false
+//         : test(counter - 1);
+//     }
+//     return test(counter);
+// }
+// test_two_digit(2000);
