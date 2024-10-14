2f77ad5aef7fb0c27ece8c6d5266597e46f9e631
Source Academy User <No public email provided>
2024-10-15 06:48:53 +0800

    Changes made from Source Academy

--- sicp_ex_1.16.js
+++ sicp_ex_1.16.js
@@ -1,1 +1,16 @@
+function is_even(n) {
+    return n % 2 === 0;
+}
 
+function expn_iter(b, n) {
+    function iter(a, b, n) {
+        return n === 1
+        ? a
+        : is_even(n)
+        ? iter(b * b, a, n / 2)
+        : a * iter(b * b, a, n - 1);
+    }
+    return iter(1, b, n);
+}
+
+expn_iter(4, 3);
