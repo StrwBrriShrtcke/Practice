fc0a01e966e009caf5c20fa853ec32df18952063
Source Academy User <No public email provided>
2024-10-14 19:26:08 +0800

    Changes made from Source Academy

--- sicp_ex_1.13.js
+++ sicp_ex_1.13.js
@@ -1,1 +1,14 @@
+function fib(n) {
+    return n <= 1
+    ? n
+    : fib(n - 1) + fib(n - 2);
+}
 
+function prove(n) {
+    return math_round(fib(n)) === 
+    math_round(math_pow(((1 + math_sqrt(5)) / 2), n) / math_sqrt(5))
+    ? true
+    : false;
+}
+
+prove(9);
