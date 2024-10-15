8370bab31a46fb0e477c0c2bcbe4768789c4f101
Source Academy User <No public email provided>
2024-10-15 12:53:31 +0800

    Fixed an issue

--- sicp_ex_1.16.js
+++ sicp_ex_1.16.js
@@ -12,7 +12,7 @@ function expn_iter(b, n) {
         ? a
         : is_even(n)
         ? iter(a, square(b), n / 2) 
-        : a * b * (iter(a, b, n - 1)); 
+        : (iter(a * b, b, n - 1)); 
     }
     return iter(1, b, n);
 }
@@ -22,5 +22,7 @@ display(expn_iter(10, 3));//1000
 display(expn_iter(3, 3));//1000
 display(expn_iter(10, 0)); // 1
 display(expn_iter(0, 10)); //0
+display(expn_iter(2, 14)); //16384
+
 
 
