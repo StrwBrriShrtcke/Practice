945077d1aecca7035e36a52235f9b66c1daa4786
Source Academy User <No public email provided>
2024-10-15 14:17:05 +0800

    Cleaned up BRACKETS

--- sicp_ex_1.17.js
+++ sicp_ex_1.17.js
@@ -10,23 +10,31 @@ function halve(x) {
     return x / 2;
 }
 
-function mult_iter(a, b) {
-    function iter(a, b) {
-        return b === 1
-        ? a
-        : a === 0 || b === 0
-        ? 0
-        : is_even(b)
-        ? iter(double(a), halve(b)) 
-        : a + (iter(a, b - 1)); 
-    }
-    return iter(a, b);
+function mult_fast(a, b) {
+    return b === 0 || a === 0
+    ? 0
+    : is_even(b)
+    ? double(mult_fast(a, halve(b))) 
+    : a + mult_fast(a, b - 1); 
 }
 
-display(mult_iter(3, 8)); //24
-display(mult_iter(3, 10)); //30
-display(mult_iter(10, 3)); //30
-display(mult_iter(3, 3));  //9
-display(mult_iter(10, 0)); //0
-display(mult_iter(0, 10)); //0
-display(mult_iter(2, 14)); //28
+display(mult_fast(3, 8));  //24
+display(mult_fast(3, 10)); //30
+display(mult_fast(10, 3)); //30
+display(mult_fast(3, 3));  //9
+display(mult_fast(10, 0)); //0
+display(mult_fast(0, 10)); //0
+display(mult_fast(2, 14)); //28
+
+
+// function fast_expt(b, n) {
+//     return n === 0
+//           ? 1
+//           : is_even(n)
+//           ? square(fast_expt(b, n / 2))
+//           : b * fast_expt(b, n - 1);
+// }
+
+
+
+
