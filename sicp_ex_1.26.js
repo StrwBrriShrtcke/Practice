fafc5039f20636f8686b42808e6602307b4bf605
Source Academy User <No public email provided>
2024-10-21 16:22:41 +0800

    Amended and completed SICP Ex 1.26

--- sicp_ex_1.26.js
+++ sicp_ex_1.26.js
@@ -30,12 +30,17 @@ expmod(4, 2, 5);
 // square which takes the value evaluated in expmod and multiplies it by 
 // itself. In doing so, it eliminates the need to evaluate expmod twice, 
 // halfing the number of steps taken and hence making the time complexity 
-// Θ(logn). This allows the program to run faster than a program that has 
+// Θ(logn). In the case of exp === 2, the time complexity of original code 
+// is Θlog_2n.This allows the program to run faster than a program that has 
 // time complexity of Θ(n).
 
 // In the case of the funciton written by Louis, by use an explicit 
 // multiplication rather than calling the funcrtion square, expmod 
 // needs to be evaluated twice in lines 22 and 23. This is inefficient 
-// as both cases where expmod is being evaluated, it returns the same 
+// as in both cases where expmod is being evaluated, it returns the same 
 // value. This also results in the time complexity of Louis's function 
-// to be Θ(n) which is why he finds that his program seems to fun slowly
+// to be twice that of the original code, making its time complexity
+// Θ2(log_2n Θ(n)) which when simplified is Θ(n) which proves Eva's point
+// So instead of writing a program with a logarithmic time complexity, 
+// Louis has written a program with a linear time complexity proportional 
+// to n, which is why he finds that his program seems to fun slowly.
