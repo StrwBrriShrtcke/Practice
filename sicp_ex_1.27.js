04ca8362ffea0f022a62926f11a35d26fe341abb
Source Academy User <No public email provided>
2024-10-22 10:31:24 +0800

    Completed SICP Ex 1.27

--- sicp_ex_1.27.js
+++ sicp_ex_1.27.js
@@ -18,16 +18,33 @@ function random(n) {
     return math_floor(math_random() * n);
 }
 
+// function fermat_test(n) {
+//     function try_it(a) {
+//         return a === n
+//         ? true
+//         : try_it(expmod(a, n, n) + 1) === a + 1;
+//     }
+//     return try_it(1);
+// }
+
+// function fermat_test(n) {
+//     function try_it(a) {
+//         return a === n || expmod(a, n, n) === a
+//         ? true
+//         : try_it(a + 1);
+//     }
+//     return try_it(2);
+// }
+
 function fermat_test(n) {
     function try_it(a) {
-        return a === n
-        ? true
-        : try_it(expmod(a, n, n) + 1) === a + 1
+        return a === n || expmod(a, n, n) === a && try_it(a + 1);
     }
-    return try_it(1)
+    return try_it(2);
 }
 
-fermat_test(561);
+//fermat_test(561);
+//fermat_test(6);
 
 // function carmichael_number(n) {
 //     function test_carmichael_number(exp, counter) {
@@ -37,12 +54,12 @@ fermat_test(561);
 //     }
 // }
 
-//display(carmichael_number(5));
-//display(carmichael_number(561));
-//display(carmichael_number(1105));
-// display(carmichael_number(1729));
-// display(carmichael_number(2465));
-// display(carmichael_number(2821));
-// display(carmichael_number(6601));
+display(fermat_test(5));
+display(fermat_test(561));
+display(fermat_test(1105));
+display(fermat_test(1729));
+display(fermat_test(2465));
+display(fermat_test(2821));
+display(fermat_test(6601));
 // Carmichael numbers
 //561, 1105, 1729, 2465, 2821, and 6601
