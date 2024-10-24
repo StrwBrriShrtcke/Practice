df753fa3188254d631aba79a8141434be495eea7
Source Academy User <No public email provided>
2024-10-24 17:19:57 +0800

    Changes made from Source Academy

--- sicp_ex_1.28.js
+++ sicp_ex_1.28.js
@@ -14,8 +14,8 @@ function expmod(base, exp, m) {
         // return square(expmod(base, exp / 2, m)) % m;
         if (exponential_modulo === 1 || exponential_modulo === m - 1) {
             return 0;
-        } else if () {
-            return ;
+        } else {
+            return square(exponential_modulo) % m;
         }
     } else {
         return (base * expmod(base, exp - 1, m)) % m;
@@ -31,7 +31,7 @@ function fermat_test(n) {
     function try_it(a) {
         return expmod(a, n - 1, n) === 0;
     }
-    return try_it(2);
+    return try_it(1 + math_floor(math_random() * (n - 1)));
 }
 
 // function fast_is_prime(n, times) {
@@ -42,4 +42,7 @@ function fermat_test(n) {
 //           : false;
 // }
 
-fermat_test(561);
+display(fermat_test(8));
+//display(fermat_test(561));
+//display(fermat_test(3));
+//display(fermat_test(5));
