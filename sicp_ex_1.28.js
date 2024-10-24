b41afb5beb076d8fbdcc0f6789c08883763b3659
Source Academy User <No public email provided>
2024-10-24 23:36:05 +0800

    Completed

--- sicp_ex_1.28.js
+++ sicp_ex_1.28.js
@@ -11,7 +11,6 @@ function expmod(base, exp, m) {
         return 1;
     } else if (is_even(exp)) {
         const exponential_modulo = expmod(base, exp / 2, m);
-        // return square(expmod(base, exp / 2, m)) % m;
         if (exponential_modulo === 1 || exponential_modulo === m - 1) {
             return 0;
         } else {
@@ -20,20 +19,48 @@ function expmod(base, exp, m) {
     } else {
         return (base * expmod(base, exp - 1, m)) % m;
     }
-    // return exp === 0
-    //       ? 1
-    //       : is_even(exp)
-    //       ? square(expmod(base, exp / 2, m)) % m
-    //       : (base * expmod(base, exp - 1, m)) % m;
 }
 
 function fermat_test(n) {
     function try_it(a) {
         return expmod(a, n - 1, n) === 0;
     }
-    return try_it(1 + math_floor(math_random() * (n - 1)));
+    return try_it(2 + math_floor(math_random() * (n - 3)));
 }
 
+function miller_rabin_test(n, a) {
+    return expmod(a, n - 1, n) === 0;
+}
+
+function test_all(n) {
+    function iter_test_all(a) {
+        // return !miller_rabin_test(n, a) 
+        // ? false
+        // : a === n - 2
+        // ? true
+        // : iter_test_all(a + 1); 
+        // if (!miller_rabin_test(n, a)) {
+        //     return false;
+        // } else if (a === n - 2) {
+        //     return true;
+        // } else {
+        //     return iter_test_all(a + 1); 
+        // }
+        return miller_rabin_test(n, a) && (a === n - 2 || 
+        iter_test_all(a + 1));
+    }
+    return iter_test_all(2); 
+}
+display(test_all(34));
+display(test_all(100));
+display(test_all(35));
+display(test_all(1105));
+display(test_all(2821));
+display(test_all(43));
+display(test_all(5));
+display(test_all(1009));
+display(test_all(13));
+display(test_all(561));
 // function fast_is_prime(n, times) {
 //     return times === 0
 //           ? true
@@ -42,7 +69,7 @@ function fermat_test(n) {
 //           : false;
 // }
 
-display(fermat_test(8));
+//display(fermat_test(6));
 //display(fermat_test(561));
 //display(fermat_test(3));
 //display(fermat_test(5));
