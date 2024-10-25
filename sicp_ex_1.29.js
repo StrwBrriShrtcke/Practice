63207280ade136a3a1d0b4b60acce153416d905f
Source Academy User <No public email provided>
2024-10-25 22:53:36 +0800

    Completed

--- sicp_ex_1.29.js
+++ sicp_ex_1.29.js
@@ -1,1 +1,21 @@
+function sum(term, a, next, b) {
+    return a > b
+           ? 0
+           : term(a) + sum(term, next(a), next, b);
+}
 
+const cube = x => x * x * x;
+
+function simpson_integral(f, a, b, n) {
+    const h = (b - a) / n;
+    const multiple_of_y = k => k === 0 || k === n 
+    ? 1 : k % 2 === 0 ? 2 : 4;
+    return (h / 3) * sum(k => f(a + k * h) * multiple_of_y(k), 
+    0, 
+    k => k + 1, 
+    n);
+    //k => k + 1 is an anonymous function 
+}
+
+display(simpson_integral(cube, 0, 1, 100));
+display(simpson_integral(cube, 0, 1, 1000));
