c27af3877a6e9cd28c4551a397c6eb85d08ab47d
Source Academy User <No public email provided>
2024-10-25 23:17:21 +0800

    To do tmr

--- sicp_ex_1_30.js
+++ sicp_ex_1_30.js
@@ -1,1 +1,9 @@
+function sum(term, a, next, b) {
+    return a > b
+           ? 0
+           : term(a) + sum(term, next(a), next, b);
+}
 
+function sum_iter(term, a, next, b) {
+    function iter()
+}
