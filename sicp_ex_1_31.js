ce5ab8b750b3bf0a5c92c256bda3b6e8df8b42cd
Source Academy User <No public email provided>
2024-10-26 15:34:38 +0800

    Pi

--- sicp_ex_1_31.js
+++ sicp_ex_1_31.js
@@ -1,1 +1,41 @@
+// Defining Factorial
+function product_iter(term, a, next, b) {
+    function iter(a, product) {
+        return a > b
+        ? product
+        : iter(next(a), term(a) * product);
+    }
+    return iter(a, 1);
+}
 
+function factorial(b) {
+    return product_iter(x => x, 1, a => a + 1, b);
+}
+
+// factorial(7);
+
+// approximate pi
+function approximate_pi_1(n) {
+    display("numerator");
+    const numerator = 2 * product_iter(x => x * x, 4, a => a + 2, n);
+    display("denominator");
+    const denominator = product_iter(x => x * x, 3, a => a + 2, n);
+    return n % 2 !== 0
+    ? (numerator * (n + 1) / denominator) * 4
+    : (numerator / (denominator * (n + 1))) * 4;
+}
+display(approximate_pi_1(99));
+// display(approximate_pi(18));
+// display(approximate_pi(19));
+// display(approximate_pi(20));
+
+function approximate_pi(n) {
+    function get_fraction(i) {
+        const numerator = (1 + math_floor(i / 2)) * 2;
+        const denominator = i % 2 === 0 ? numerator - 1 : numerator + 1;
+        return numerator / denominator;
+    }
+    return product_iter(get_fraction, 1, i => i + 1, n) * 4;
+}
+
+approximate_pi(99);
