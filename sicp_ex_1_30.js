5fc7e8b899bc4ab0cd65a5a9ed833a5fdcacae20
Source Academy User <No public email provided>
2024-10-26 12:04:53 +0800

    Attempted

--- sicp_ex_1_30.js
+++ sicp_ex_1_30.js
@@ -1,9 +1,21 @@
-function sum(term, a, next, b) {
-    return a > b
-           ? 0
-           : term(a) + sum(term, next(a), next, b);
+function cube(x) {
+    return x * x * x;
 }
 
 function sum_iter(term, a, next, b) {
-    function iter()
+    function iter(a, sum) {
+        return a > b
+        ? sum
+        : iter(next(a), term(a) + sum);
+    }
+    return iter(a, 0);
 }
+
+function inc(n) {
+    return n + 1;
+}
+function sum_cubes(a, b) {
+    return sum_iter(cube, a, inc, b);
+}
+
+sum_cubes(1, 10);
