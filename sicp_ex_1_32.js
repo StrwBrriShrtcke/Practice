dbd82674d72e4904911e8690052747d197ae611a
StrwBrriShrtcke <Leejiawei241@gmail.com>
2024-10-27 09:44:33 +0800

    Added sum and product function

--- sicp_ex_1_32.js
+++ sicp_ex_1_32.js
@@ -1,1 +1,19 @@
+// Product function
+function product_iter(term, a, next, b) {
+    function iter(a, product) {
+        return a > b
+        ? product
+        : iter(next(a), term(a) * product);
+    }
+    return iter(a, 1);
+}
 
+// Sum function
+function sum_iter(term, a, next, b) {
+    function iter(a, sum) {
+        return a > b
+        ? sum
+        : iter(next(a), term(a) + sum);
+    }
+    return iter(a, 0);
+}
