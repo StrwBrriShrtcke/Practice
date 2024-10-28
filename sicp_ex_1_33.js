29f50d9e8b3fd7a6cf2f63cd45dea5ef72bbefdb
Source Academy User <No public email provided>
2024-10-28 21:26:16 +0800

    Completed

--- sicp_ex_1_33.js
+++ sicp_ex_1_33.js
@@ -1,40 +1,68 @@
-function expmod(base, exp, m) {
-    if (exp === 0) {
-        return 1;
-    } else if (exp % 2 === 0) {
-        const exponential_modulo = expmod(base, exp / 2, m);
-        if (exponential_modulo === 1 || exponential_modulo === m - 1) {
-            return 0;
-        } else {
-            return (exponential_modulo * exponential_modulo) % m;
-        }
-    } else {
-        return (base * expmod(base, exp - 1, m)) % m;
-    }
+function square(x) {
+    return x * x;
 }
 
-function miller_rabin_test(n, a) {
-    return expmod(a, n - 1, n) === 0;
+function smallest_divisor(n) {
+    return find_divisor(n, 2);
+}
+function find_divisor(n, test_divisor) {
+    return square(test_divisor) > n
+           ? n
+           : divides(test_divisor, n)
+           ? test_divisor
+           : find_divisor(n, test_divisor + 1);
+}
+function divides(a, b) {
+    return b % a === 0;
 }
 
-function test_all(n) {
-    function iter_test_all(a) {
-        return miller_rabin_test(n, a) && (a === n - 2 || 
-        iter_test_all(a + 1));
-    }
-    return iter_test_all(2); 
+function is_prime(n) {
+    return n === smallest_divisor(n);
+}
+// Relative prime function
+
+function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b);
 }
 
+// Filtered accumulate function
 function filtered_accumulate(combiner, null_value, term, a, next, b, 
     filter) {
     function iter(a, accumulated_value) {
         return a > b
         ? accumulated_value
-        : !filter(a)
-        ? iter(a + 1, accumulated_value)
-        : iter(next(a), combiner(accumulated_value, term(a)));
+        : filter(a)
+        ? iter(next(a), combiner(accumulated_value, term(a)))
+        : iter(a + 1, accumulated_value);
     }
     return iter(a, null_value);
 }
 
-display(filtered_accumulate((sum, value) => sum  + (value * value), 0, x => x, 10, x => x + 1, 20, test_all));
+display(filtered_accumulate((sum, value) => sum  + (value * value), 
+0, 
+x => x, 
+10, 
+x => x + 1, 
+20, 
+is_prime));
+
+display(filtered_accumulate((product, value) => product * value, 
+1, 
+x => x, 
+1, 
+x => x + 1, 
+10 - 1, 
+x => gcd(x, 10) === 1));
+
+// Function that evaluates the product of numbers relatively prime to n 
+// and < n
+function part_2(n) {
+    return filtered_accumulate((product, value) => product * value, 
+            1, 
+            x => x, 
+            1, 
+            x => x + 1, 
+            n - 1, 
+            x => gcd(x, n) === 1);
+}
+display(part_2(10));
