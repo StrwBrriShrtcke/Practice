84202d12251501c9bf2e1eea9c2c179b2d3593cb
Source Academy User <No public email provided>
2024-10-28 20:41:14 +0800

    Completed

--- sicp_ex_1_32.js
+++ sicp_ex_1_32.js
@@ -18,23 +18,46 @@ function sum_iter(term, a, next, b) {
     return iter(a, 0);
 }
 
-function accumulate(combiner, null_value, term, a, next, b) {
-    function iter(a, accumulated_value) {
-        display(accumulated_value);
-        return a > b
-        ? accumulated_value
-        : iter(next(a), combiner(accumulated_value, term(a)));
-    }
-    return iter(a, null_value);
+// function accumulate(combiner, null_value, term, a, next, b) {
+//     function iter(a, accumulated_value) {
+//         display(accumulated_value);
+//         return a > b
+//         ? accumulated_value
+//         : iter(next(a), combiner(accumulated_value, term(a)));
+//     }
+//     return iter(a, null_value);
+// }
+
+function accumulate_recur(combiner, null_value, term, a, next, b) {
+    display(term);
+    return a > b
+    ? null_value
+    : combiner(null_value, 
+    accumulate_recur(combiner, 
+    term(a), 
+    term, 
+    next(a), 
+    next, 
+    b));
 }
 
 // 1 + 2 + 3 + ...+ 10
-//display(accumulate((sum, value) => sum + value, 0, x => x, 1, x => x + 1, 10));
+display(accumulate_recur((sum, value) => {
+    //display(sum);
+    //display(value);
+    return sum + value;
+    
+}, 
+    0, 
+    x => x, //term
+    1, 
+    x => x + 1, 
+    10));
 // 10 * 20 * 30 *...* 100
 //display(accumulate((product, value) => product * value, 1, x => x, 10, x => x + 10, 100));
 // "2468101214161820"
 //display(accumulate((string, value) => string + value, "", stringify,
 //2, x => x + 2, 20));
 //100 - 1 - 2 - 3 -... - 10
-display(accumulate((x, y) =>  x - y, 100, x => x, 1,
-x => x + 1, 10));
+// display(accumulate_recur((x, y) =>  x - y, 100, x => x, 1,
+// x => x + 1, 10));
