1f2255e84f2e54183e91b3e58aa8b5e4d235f338
Source Academy User <No public email provided>
2024-10-29 10:07:01 +0800

    Completed

--- sicp_ex_1_34.js
+++ sicp_ex_1_34.js
@@ -1,1 +1,19 @@
-sico
+function f(g) {
+   return g(2);
+}
+
+function square(x) {
+    return x * x;
+}
+
+f(square);
+
+f(z => z * (z + 1));
+
+f(f);
+// (2)(2);
+
+// The function f expects a function as an argument. When passing f as
+// an argument in the function f, it takes f as the value 2 when 
+// trying to execute return, this leads to function f calling 2 as
+// the function g resulting in (2)(2).
