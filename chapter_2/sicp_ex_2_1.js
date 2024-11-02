03cd55417fdfb77e5426ca9e5f0075f708cdb334
Source Academy User <No public email provided>
2024-11-02 11:00:13 +0800

    Completed

--- chapter_2/sicp_ex_2_1.js
+++ chapter_2/sicp_ex_2_1.js
@@ -1,1 +1,22 @@
+function numer(x) {
+    return head(x);
+}
+function denom(x) {
+    return tail(x);
+}
 
+function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b);
+}
+
+function make_rat(n, d) {
+    const g = gcd(n, d);
+    return d < 0 
+    ? make_rat(- n, - d)
+    : pair(n / g, d / g);
+}
+
+make_rat(4, -6);
+
+//if both num and den is neg make pos, if either den or num is neg, make 
+// num neg
