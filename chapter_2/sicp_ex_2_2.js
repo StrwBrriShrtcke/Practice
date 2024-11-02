82fd217bae3e80e1926b571dda154ebaba02d78e
Source Academy User <No public email provided>
2024-11-02 11:08:19 +0800

    Changes made from Source Academy

--- chapter_2/sicp_ex_2_2.js
+++ chapter_2/sicp_ex_2_2.js
@@ -1,1 +1,9 @@
 
+// make point which is original make rat
+
+// make segment is a pair of pairs end and start segment
+
+// midpoint will take in a segment and return the midpoint of the segment
+
+
+display(midpoint(segment))
