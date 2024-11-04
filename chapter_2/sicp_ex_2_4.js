dd8683fe4e6c7bad2b02fc58642b8012c20fdb9f
Source Academy User <No public email provided>
2024-11-04 16:40:48 +0800

    Attempted

--- chapter_2/sicp_ex_2_4.js
+++ chapter_2/sicp_ex_2_4.js
@@ -1,1 +1,16 @@
+function pair(x, y) {
+    return m => m(x, y);
+}
+function head(z) {
+    return z((p, q) => p);
+}
 
+const x = pair(1, 2);
+head(x);
+
+function tail(z) {
+    return z((p, q) => q);
+}
+
+const y = pair(1, 2);
+tail(y);
