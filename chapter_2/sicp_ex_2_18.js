00967a5cecec32177b5b78a930ae9694ae4622bb
Source Academy User <No public email provided>
2024-11-10 10:16:52 +0800

    Attempted

--- chapter_2/sicp_ex_2_18.js
+++ chapter_2/sicp_ex_2_18.js
@@ -1,1 +1,17 @@
+function reverse_list(list1) {
+    function iter(remaining, reversed) {
+        return is_null(remaining)
+        ? reversed
+        : iter((tail(remaining)), pair(head(remaining), reversed));
+    }
+  return iter(list1, list());
+}
+
+
+
+
+reverse_list(list(1, 4, 9, 16, 25)); 
+
+
+
 
