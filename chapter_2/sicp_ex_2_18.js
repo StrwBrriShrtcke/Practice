a5f6f5728f4176c0abd70598ffb6c7974aaf0310
Source Academy User <No public email provided>
2024-11-12 10:09:05 +0800

    Completed

--- chapter_2/sicp_ex_2_18.js
+++ chapter_2/sicp_ex_2_18.js
@@ -13,5 +13,15 @@ function reverse_list(list1) {
 reverse_list(list(1, 4, 9, 16, 25)); 
 
 
+function slow_reverse(this_list) {
+    display(this_list);
+    return is_null(this_list) 
+    ? null
+    : append(slow_reverse(tail(this_list)),
+             list(head(this_list)));
+}
+
+slow_reverse(list(1,2,3,4)); // [4, [3, [2, [1, null]]]]
+
 
 
