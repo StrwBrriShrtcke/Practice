02ca0d8b85d4c3be5d61012b1a1907f07b73c8d9
Source Academy User <No public email provided>
2024-11-12 11:01:15 +0800

    Completed

--- chapter_2/sicp_ex_2_19.js
+++ chapter_2/sicp_ex_2_19.js
@@ -1,17 +1,20 @@
-const us_coins = list(50, 25, 10, 5, 1);
-const uk_coins = list(100, 50, 20, 10, 5, 2, 1);
+const no_more = is_null; 
+
+const except_first_denomination = tail; 
+
+const first_denomination = head; 
 
 function cc(amount, coin_values) {
     return amount === 0
-           ? 1
-           : amount < 0 || no_more(coin_values)
-           ? 0
-           : cc(amount, except_first_denomination(coin_values)) +
+          ? 1
+          : amount < 0 || no_more(coin_values)
+          ? 0
+          : cc(amount, except_first_denomination(coin_values)) +
              cc(amount - first_denomination(coin_values), coin_values);
 }
-
-cc(100, us_coins);
 
 
 const us_coins = list(50, 25, 10, 5, 1);
 const uk_coins = list(100, 50, 20, 10, 5, 2, 1);
+
+cc(100, us_coins);
