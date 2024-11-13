f627de1dbfd409190c35ff61ebeab2d776703ad9
Source Academy User <No public email provided>
2024-11-14 06:03:25 +0800

    Attempted

--- chapter_2/sicp_ex_2_21.js
+++ chapter_2/sicp_ex_2_21.js
@@ -1,1 +1,17 @@
+// function that takes a list of arguments and returns a list of
+// the squares of the arguments
 
+function square_list(items) {
+    return is_null(items) 
+           ? null
+           : pair(head(items) * head(items), square_list(tail(items)));
+
+}
+      
+square_list(list(1, 2, 3, 4));
+
+function square_list_2(items) {
+  return map(x => x * x, items);
+}
+
+square_list_2(list(1, 2, 3, 4));
