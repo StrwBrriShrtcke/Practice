4d6c7d6112e5e300bc80a4c2a3aa3fcb6cf798cb
Source Academy User <No public email provided>
2024-11-14 23:21:47 +0800

    Changes made from Source Academy

--- chapter_2/sicp_ex_2_23.js
+++ chapter_2/sicp_ex_2_23.js
@@ -1,1 +1,14 @@
+// create a function that is similar to predeclared for_each
 
+function abs(x) {
+    return x >= 0 ? x : - x;
+}
+
+function map(fun, items) {
+    return is_null(items)
+           ? null
+           : pair(fun(head(items)), 
+                  map(fun, tail(items)));
+}
+
+for_each(x => display(x), list(57, 321, 88));
