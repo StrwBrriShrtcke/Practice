b059908a4d8b9369b9535368517955c3e40576c2
Source Academy User <No public email provided>
2024-11-14 16:47:23 +0800

    completed

--- chapter_2/sicp_ex_2_21.js
+++ chapter_2/sicp_ex_2_21.js
@@ -1,18 +1,16 @@
-// My attempt at making square_list more efficient as i realised that 
-// in the current square_list function, it evaluates head(items) twice.
-// But here i ran into an issue where in the final iteration head(items)
-// would evaluate head(null) which is not a pair and hence stops evaluating
-// and returns an error
+// square_list defined such that head(items) only gets evaluated once
 
 function square_list(items) {
-    const element = head(items);
-    display(element);
-    return is_null(items)
-           ? null
-           : pair(element * element, square_list(tail(items)));
+     if (is_null(items)) {
+         return null;
+     } 
+     else {
+         const element = head(items);
+         return pair(element * element, square_list(tail(items)));
+     }
 }
 
-square_list(list(1, 2, 3, 4));
+display(square_list(list(1, 2, 3, 4)));
 
 // function that takes a list of arguments and returns a list of
 // the squares of the arguments
@@ -20,7 +18,7 @@ square_list(list(1, 2, 3, 4));
 function square_list_1(items) {
     return is_null(items)
            ? null
-           : pair(head(items) * head(items, square_list(tail(items)));
+           : pair(head(items) * head(items), square_list(tail(items)));
 }
 square_list_1(list(1, 2, 3, 4));
 
