b28b52f796f345b665a31235bb1bb93f346abba2
Source Academy User <No public email provided>
2024-11-15 19:50:47 +0800

    Completed

--- chapter_2/sicp_ex_2_23.js
+++ chapter_2/sicp_ex_2_23.js
@@ -1,23 +1,12 @@
 // create a function that is similar to predeclared for_each
 
-function abs(x) {
-    return x >= 0 ? x : - x;
-}
-
-function map(fun, items) {
-    return is_null(items)
-           ? null
-           : pair(fun(head(items)), 
-                  map(fun, tail(items)));
-}
-
-function for_each_imp(fun, items) {
-    function iter(element, remaining) {
-        return is_null(remaining)
-        ? 1
-        : iter(fun(head(remaining)), tail(remaining));
+function for_each_1(fun, items) {
+    if (!is_null(items)) {
+        fun(head(items));
+        for_each_1(fun, tail(items));
+    } else {
+        
     }
-    return iter(1, items);
 }
 
-display(for_each_imp(x => display(x), list(57, 321, 88)));
+display(for_each_1(x => display(x), list(57, 321, 88)));
